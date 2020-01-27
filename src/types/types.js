0f47f9c8e99b6d54f5146260dbe4d57715e99ac8
William Swanson <swansontec@gmail.com>
2020-01-27 13:12:10 -0800

    Fix public types (wip)

--- src/types/types.js
+++ src/types/types.js
@@ -1078,7 +1078,10 @@ export type EdgeContext = {
   changePaused(
     paused: boolean,
     opts?: { secondsDelay?: number }
-  ): Promise<mixed>
+  ): Promise<mixed>,
+
+  // Deprecated API's:
+  pinExists(username: string): Promise<boolean>
 }
 
 // ---------------------------------------------------------------------
@@ -1089,8 +1092,8 @@ export type EdgeFakeUser = {
   username: string,
   loginId: string,
   loginKey: string,
-  repos: { [repo: string]: { [path: string]: JsonObject } },
-  server: JsonObject
+  repos: { [repo: string]: { [path: string]: any /* JsonBox */ } },
+  server: any /* DbLogin & { children?: DbLogin[] } */
 }
 
 export type EdgeFakeWorld = {
